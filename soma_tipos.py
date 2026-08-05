9ee4c45ffb674cbc2134315508068008acc72932
larijbpg <larissa.jbprgm@gmail.com>
2026-08-05 19:39:42 -0300

    Adiciona exercícios de tipos primitivos

---
+++ soma_tipos.py
@@ -0,0 +1,22 @@
+# Criar um script em Python que leia dois numeros e tente mostrar a SOMA deles
+# n1= input('primeiro numero')
+# n2 =input('segundo numero')
+# x = n1 + n2
+# print('A soma vale', x)
+# print( type(x))
+# A soma vale 13 ----> CONCATENAÇÃO
+# <class 'str'>
+
+                                            # Tipos primitivos
+
+                                            # int() = numero inteiro -> 7, -4, 2550
+                                            # float() = numero com casas decimais -> 7.0, -7.0, 0.0075
+                                            # str() = texto - 'Olá', '7.5', ''= texto vazio
+                                            # bool() = valores lógicos - True ou False
+
+
+n1 = int(input('primeiro numero:'))
+n2= int(input('segundo numero:'))
+x = n1 + n2
+print('A soma vale', x)
+print(type(x))
